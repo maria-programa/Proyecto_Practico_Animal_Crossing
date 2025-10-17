87c3823202f251645171f8dc49403ddfb6670e5d
Maria Lozano <mlozanoovejero314@gmail.com>
2025-10-17 09:42:20 +0200

    (feature): creación del proyecto

---
+++ Guia Animal Crossing New Horizons/Guia Animal Crossing New Horizons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guia Animal Crossing New Horizons
+//
+//  Created by Maria Lozano Ovejero on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guia Animal Crossing New Horizons/Guia Animal Crossing New Horizons/Guia_Animal_Crossing_New_HorizonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guia_Animal_Crossing_New_HorizonsApp.swift
+//  Guia Animal Crossing New Horizons
+//
+//  Created by Maria Lozano Ovejero on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Guia_Animal_Crossing_New_HorizonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
